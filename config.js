2abdab814b6ff7e50dc698c0c57bafcf26e2d2dd
Megan Levin <megan@melange.com>
2026-08-04 23:39:44 -0400

    Connect snorse-bot Ko-fi page

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.SNORSE_CONFIG = {
   signalUsername: "snorse-bot",
   capacityApi:
     "https://6pzf362hlwhwgczdtpksiaoa6m0becqj.lambda-url.us-east-1.on.aws/api/public/capacity",
-  kofiUrl: "",
+  kofiUrl: "https://ko-fi.com/snorsebot",
   projectUrl: "https://github.com/mlevin5/snorse-bot",
 };
